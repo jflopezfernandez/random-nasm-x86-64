61be291de841e5d7378e403152fd456b2c914fd0
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2019-06-07 15:30:03 -0400

    Refactored and rewrote some sections.

--- Makefile
+++ Makefile
@@ -3,27 +3,30 @@ vpath %.asm src
 vpath %.inc include
 
 OBJS        = main.o
+LSTS        = $(patsubst %.o,%.lst,$(OBJS))
 
 AS          = nasm
-ASFLAGS     = -Wall -f elf64 -l main.lst -O0 -g -F dwarf
+ASFLAGS     = -Wall -f elf64
 
 LD          = ld
-LDFLAGS     = --strip-all --format elf64-x86-64 --warn-common --gc-sections
-
-CC          = gcc-9.1.0
-CFLAGS      = 
-CPPFLAGS    = 
+LDFLAGS     = -b elf64-x86-64
 
 TARGET      = random
 
 all: $(TARGET)
 
-$(TARGET): main.o
-	$(LD) $(LDFLAGS) -o $@ $<
+$(TARGET): $(OBJS)
+	$(LD) $(LDFLAGS) -o $@ $^
 
 %.o: %.asm
-	$(AS) $(ASFLAGS) -o $@ $<
+	$(AS) $(ASFLAGS) -o $@ $^
+
+listings: $(LSTS)
+
+%.lst: %.asm
+	$(AS) $(ASFLAGS) -l $@ $^
 
 .PHONY: clean
 clean:
-	$(RM) $(wildcard *.o) $(wildcard *.pp) $(wildcard *.lst) $(TARGET)
+	$(RM) $(OBJS) $(LSTS) $(TARGET)
+
--- src/main.asm
+++ src/main.asm
@@ -33,52 +33,105 @@
 ;
 ;
 ;==============================================================================
+;
+;                           PREPROCESSOR DEFINITIONS
+;
+;==============================================================================
 
-            global      _start
+    %define     FALSE           0
+    %define     TRUE            1
 
-            SECTION     .data
-ver:        db          "Random - Version 2.1.1", 10, 0
-verlen:     equ         $-ver
-author:     db          "Author: Jose Fernando Lopez Fernandez", 10, 0
-authlen:    equ         $-author
-nl:         db          10
+    %define     SYSCALL_READ    0
+    %define     SYSCALL_WRITE   1
+    %define     SYSCALL_EXIT    60
 
-            SECTION     .text
-_start:     push        rbp
-            mov         rbp, rsp
-            xor         rcx, rcx
-            mov         r9, 10
-.get_rand:  rdrand      rax
-            cmp         rax, 0
-            je          .get_rand
-            jg          .get_digit
-            imul        rax, -1
-.get_digit: cmp         rax, 0
-            jz          .print
-            inc         rcx
-            xor         rdx, rdx
-            div         r9
-            add         rdx, 48     ; Convert to ASCII
-            push        rdx
-            jmp         .get_digit
-.print:     jrcxz       .final_nl
-            mov         rax, 1
-            mov         rdi, 1
-            mov         rsi, rsp
-            mov         rdx, 1
-            push        rcx
-            syscall
-            pop         rcx
-            pop         rax         ; Dump into garbage register
-            dec         rcx
-            jmp         .print
-.final_nl:  mov         rax, 1
-            mov         rdi, 1
-            mov         rsi, nl
-            mov         rdx, 1
-            syscall
-.call_exit: pop         rbp
-            mov         rbp, rsp
-            mov         rax, 60
-            xor         rdi, rdi
-            syscall
+    %define     STDIN           0
+    %define     STDOUT          1
+    %define     STDERR          2
+
+    %define     NULL            0
+
+    %define     CHAR_NEWLINE    10
+
+    %define     SECTOR_SIZE     512
+    %define     BUFSIZE         SECTOR_SIZE
+
+;==============================================================================
+;
+;                               TEXT SECTION
+;
+;==============================================================================
+
+                SECTION .TEXT
+                GLOBAL _start
+
+;            SECTION     .data
+;ver:        db          "Random - Version 2.1.1", 10, 0
+;verlen:     equ         $-ver
+;author:     db          "Author: Jose Fernando Lopez Fernandez", 10, 0
+;authlen:    equ         $-author
+;nl:         db          10
+
+_start:         POP     RBX                 ; Move argc into RBX
+
+                ; If RBX equals 1, there were no arguments passed in. Skip
+                ; argument parsing and checking stage.
+
+                CMP     RBX,1               ; test (argc == 1)
+                JE      .GET_RAND           ; If TRUE, skip to .GET_DIGITS
+
+                POP     RBX                 ; Overwrite RBX with &argv[0]
+.GET_NEXT_ARG:  POP     RBX                 ; ++argv
+                CMP     RBX,NULL            ; Check if arg == NULL
+                JZ      .GET_RAND           ; If argv = 0, args process. done
+
+                ; Check each argument for valid values and set settings
+
+.GET_RAND:      RDRAND  RAX                 ; Get random number
+                JNC     .GET_RAND           ; If CF=0, result invalid. Repeat.
+                CMP     RAX,0               ; Need to check if positive
+;                JG      .GET_DIGITS         ; If positive, proceed to parse
+;                MUL    RAX,-1               ; Neg value causes SIGFPE
+
+                ; Get each digit using 'MOD 10, DIV 10' algorithm.
+
+.GET_DIGITS:    MOV     R15,10              ; This is the divisor
+                PUSH    0                   ; Push NUL terminator for finish
+                                            ; condition with no counter.
+
+.GET_DIGIT:     CMP     RAX,0               ; If RAX = 0, done getting digits
+                JE      .PRINT_DIGIT        ; If RAX = 0, done
+                XOR     RDX,RDX             ; Zero out top half of dividend
+                DIV     R15                 ; Divide [RDX:RAX] by R15
+                ADD     RDX,48              ; Convert to ASCII
+                PUSH    RDX                 ; This is the current least sig dig
+                JMP     .GET_DIGIT          ; Loop back to get next digit
+
+.PRINT_DIGIT:   CMP     QWORD[RSP],0        ; TODO: Implement PRINT_DIGIT
+                JE      .FINISH             ; Found NULL terminator. goto EXIT
+                MOV     RAX,SYSCALL_WRITE
+                MOV     RDI,STDOUT
+                MOV     RSI,RSP             ; 'String' addr. = RSP
+                MOV     RDX,1               ; Single char length = 1
+                SYSCALL                     ; TODO: Check return value
+                POP     RSI                 ; Remove char off stack after print
+                JMP     .PRINT_DIGIT        ; Go print next char
+
+.FINISH:        POP     RBX                 ; Pop final char from stack
+
+                ; Print final newline
+
+                PUSH    CHAR_NEWLINE        ; Push newline char to stack
+                MOV     RAX,SYSCALL_WRITE
+                MOV     RDI,STDOUT
+                MOV     RSI,RSP             ; 'String' addr: RSP
+                MOV     RDX,1               ; Single char length = 1
+                SYSCALL                     ; Print newline
+                POP     RBX                 ; Pop newline char from stack
+
+                ; Exit program
+
+.EXIT_SUCCESS:  XOR     RDI,RDI             ; Exit code 0 (EXIT_SUCCESS)
+.EXIT:          MOV     RAX,60              ; Alow JMP to exit with code RDI
+                SYSCALL
+
